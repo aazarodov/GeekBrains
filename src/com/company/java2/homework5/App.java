d0bd42f92c257d11f658285c5a962cd14cd6d361
aazarodov <aazarodov@gmail.com>
2017-06-30 20:16:11 +0300

    java 2 homework 4

---
+++ src/com/company/java2/homework5/App.java
@@ -0,0 +1,18 @@
+package com.company.java2.homework5;
+
+public class App {
+    private static final int SIZE = 10000000;
+    private static final int H = SIZE / 2;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void method1() {
+
+    }
+
+    public static void method2() {
+
+    }
+}
